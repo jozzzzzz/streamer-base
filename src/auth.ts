d1309f3998de81db7d589d23e1fffa974cd5d058
Nao Mausservey <nmausservey@cegid.com>
2024-09-20 14:51:11 +0200

    refactor: clean up unused files in auth and main modules

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
 
-export async function authGuard(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
+export async function authGuard(_: RouteLocationNormalized, __: RouteLocationNormalized, next: NavigationGuardNext) {
   try {
     const response = await fetch('http://localhost:3000/auth/check', {
       method: 'GET',
